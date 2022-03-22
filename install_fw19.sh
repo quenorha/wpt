2ebea73d1e19707d9f200d608edfef0ce20f93e9
quenorha <51406726+quenorha@users.noreply.github.com>
2022-03-22 08:22:28 +0100

    Update install_fw19.sh

--- install_fw19.sh
+++ install_fw19.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wget https://curl.se/ca/cacert.pem -o /root/cacert.pem
-wget https://raw.githubusercontent.com/quenorha/wpt/main/curlrc -o /root/.curlrc
+wget https://curl.se/ca/cacert.pem -O /root/cacert.pem
+wget https://raw.githubusercontent.com/quenorha/wpt/main/curlrc -O /root/.curlrc
 curl https://raw.githubusercontent.com/quenorha/wpt/main/install.sh -o $PWD/install.sh -s && chmod +x $PWD/install.sh && $PWD/install.sh
 
 
